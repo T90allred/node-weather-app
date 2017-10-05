b824c1d60083ca6efd5d5783f1a984f9d2715ebe
Derek Allred <derekmallred@gmail.com>
2017-10-04 21:59:50 -0600

    understanding the call stack

---
+++ playground/async-basics.js
@@ -0,0 +1,11 @@
+console.log('Starting App');
+
+setTimeout(() => {
+  console.log('Finished here');
+}, 3000);
+
+setTimeout( () => {
+  console.log('testing');
+}, 0000);
+
+console.log('Finishing App');
